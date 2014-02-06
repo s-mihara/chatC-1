a56e94e4e0dfb233c52c324ff9cefbb3b96bf2f8
kiminori <kiminori.a@gmail.com>
2014-02-06 20:36:39 +0900

    committed in 2014/02/06 20:36:39

--- sample/developer7/sample1.js
+++ sample/developer7/sample1.js
@@ -9,7 +9,7 @@ server.on('request', function(request, response) {
   console.log('request!!');
 
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('hello rakus!!');
+  response.end('HELLO RAKUS!!!!!!!!');
 });
 
 server.listen(settings.port);
